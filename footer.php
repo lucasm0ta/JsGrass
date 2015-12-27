6536a81d0363248923991ab60ba759eb5e212303
Lucas <lmotaribeiro@gmail.com>
2015-12-27 14:21:16 -0200

    Init

---
+++ footer.php
@@ -0,0 +1,12 @@
+<div id="coroa"></div>
+<footer> 
+    <section id="ccenter">
+	        <img src="img/KDev_logo_mono_escuro.png" class="logof"> </img>
+	        <div id="contentfooter">  
+
+            <p id="all"> &copy2014 Kiwi Developments</p> 
+            <a href="http://facebook.com/kiwidev" target="_blank"><img src="img/face.png" id="icosof"></a>
+            <a href="http://twitter.com/kiwidev" target="_blank"><img src="img/twitter.png" id="icoso"></a>
+        </div>
+    </section>
+</footer>
